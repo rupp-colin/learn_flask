671780d227c30ffb4476a62d8a6296186af6cf93
Colin Rupp <colin@thinkful.com>
2019-01-16 11:38:54 -0800

    adds login function

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -9,6 +9,8 @@ from flaskr.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
+# ################## REGISTER FUNCTIONS #################### #
+
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
     if request.method == 'POST':
@@ -38,4 +40,29 @@ def register():
 
     return render_template('auth/register.html')
 
+# ################### LOGIN FUNCTION ####################### #
 
+@db.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
+
+        if user is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password.'
+
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('index'))
+
+        flash(error)
+
+    return render_template('auth/login.html')
