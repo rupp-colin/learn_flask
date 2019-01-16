65c86fc317072a1d0d7b9d9395098ae1d30bdc73
Colin Rupp <colin@thinkful.com>
2019-01-16 12:53:52 -0800

    adds annotaions and fixes formatting

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -29,29 +29,32 @@ def register():
             # validates that password exits
         elif not password:
             error = 'Password is required.'
-            # db.execute uses as many '?' as necessary for palceholders.  Second argument of
-            # db.execute fills in values for '?' in first argument.
+            # db.execute uses as many '?' as necessary for palceholders. Second
+            # argument of db.execute fills in values for '?' in first argument.
             #
-            # Validates to make sure username is not already present in the database and
-            # throws creates an error if user already exists
+            # Validates to make sure username is not already present in the
+            # database and throws creates an error if user already exists
         elif db.execute(
             'SELECT id FROM user WHERE username = ?', (username,)
         ).fetchone() is not None:
             error = 'User {} is already registered.'.format(username)
 
-        # if there is no error, create the new user with 'username' and hashed 'password'
+        # if there is no error, create the new user with 'username' and hashed
+        # 'password'
         if error is None:
             db.execute(
                 'INSERT INTO user (username, password) VALUES (?, ?)',
                 (username, generate_password_hash(password))
             )
-            # db.commit needs to be called in order to save changes (according to the tutorial)
+            # db.commit needs to be called in order to save changes (according
+            # to the tutorial)
             db.commit()
             # redirect after successful registration to instantly login user
             return redirect(url_for('auth.login'))
 
-        # flash() is a special function used to store error messages that are retrieved when
-        # rendering the template. If error != None, the error will display to the user
+        # flash() is a special function used to store error messages that are
+        # retrieved when rendering the template. If error != None, the error
+        # will display to the user
         flash(error)
 
     # render_template() calls a specific html template to be rendered
@@ -63,24 +66,33 @@ def register():
 @bp.route('/login', methods=('GET', 'POST'))
 def login():
     if request.method == 'POST':
+        # pulls username and password from the request form and access the
+        # database
         username = request.form['username']
         password = request.form['password']
         db = get_db()
         error = None
+        # looks in the database for a username with the sent of username
         user = db.execute(
             'SELECT * FROM user WHERE username = ?', (username,)
         ).fetchone()
 
+        # validation check to make sure the user exists in the databse
         if user is None:
             error = 'Incorrect username.'
+            # validation check to see if the hashed password from the form
+            # matches the db's password
         elif not check_password_hash(user['password'], password):
             error = 'Incorrect password.'
 
+        # if no error exists, clear the current session data and assign it
+        # to the new user's ID, then redirect to the 'index' page
         if error is None:
             session.clear()
             session['user_id'] = user['id']
             return redirect(url_for('index'))
 
+        # show the user any errors, if they exists
         flash(error)
 
     return render_template('auth/login.html')
@@ -88,8 +100,13 @@ def login():
 # ################## ADDS USER INFO TO SESSION ############## #
 
 
-@bp.before_app_request
+# bp.before_app_request() registers a function that runs before the view
+# function no matter what URL is requested.  This allows us to load the
+# current users data.
+@bp.before_app_first_request
 def load_logged_in_user():
+    # checks to see if a user is logged in.  Pull the user_id from the
+    # session object otherwise user is None
     user_id = session.get('user_id')
 
     if user_id is None:
@@ -112,6 +129,8 @@ def logout():
 # ################# FUNCTION FOR REQUIRING LOG IN ############ #
 
 
+# Wraps the original view in a new function that checks to see if a user
+# is logged in. If no user is logged, then redirect to the login page.
 def login_required(view):
     @functools.wraps(view)
     def wrapped_view(**kwargs):
