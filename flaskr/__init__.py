795e38451f8f20850ede1853fdf4a27420534725
Colin Rupp <colin@thinkful.com>
2019-01-16 16:04:14 -0800

    changes format

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -25,9 +25,9 @@ def create_app(test_config=None):
         pass
 
     # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, Colin! Great job making your first flask app :D'
+    @app.route('/hello/'name)
+    def hello(name):
+        return 'Hello, ' + name + '! Great job making your first flask app :D'
 
     from . import db
     db.init_app(app)
--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
 
+@app.route('/')
+def hello(name):
+    return 'Hello,' + name + '!'
